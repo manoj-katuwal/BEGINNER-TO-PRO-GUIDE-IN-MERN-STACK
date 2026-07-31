28b3e28256b3c73d10dbb573f5fb76bd40afe5ff
Manoj Katuwal <mkc684867@gmail.com>
2026-07-31 22:53:26 +0545

    compression module has been learned

---
+++ 6. Express js/compression-demo/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import compression from "compression";
+
+const app = express();
+
+app.use(compression());
+
+app.get("/users", (req, res) => {
+  const users = [];
+
+  for (let i = 1; i <= 5000; i++) {
+    users.push({
+      id: i,
+
+      name: "Ram",
+
+      age: 20,
+
+      city: "Kathmandu",
+
+      country: "Nepal",
+    });
+  }
+
+  res.json(users);
+});
+
+app.listen(3000);
