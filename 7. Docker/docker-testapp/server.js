8ceae9da16e4266beb4a8d3d08950bdc563730b7
Manoj Katuwal <mkc684867@gmail.com>
2026-08-03 18:39:02 +0545

    Successfully tested doker mongodb collection:

--- 7. Docker/docker-testapp/server.js
+++ 7. Docker/docker-testapp/server.js
@@ -5,10 +5,11 @@ const MongoClient = require("mongodb").MongoClient;
 
 const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 // const MONGO_URL = "mongodb://admin:qwerty@mongo:27017";
-const MONGO_URL = "mongodb://admin:qwerty@127.0.0.1:27017";
+const MONGO_URL = "mongodb://admin:qwerty@127.0.0.1:27017/?authSource=admin";
 const client = new MongoClient(MONGO_URL);
 
 //GET all users
@@ -17,25 +18,33 @@ app.get("/getUsers", async (req, res) => {
     await client.connect();
     const db = client.db("manoj-db");
     const data = await db.collection("users").find({}).toArray();
-    res.send(data);
+    res.json(data);
   } catch (err) {
     console.error(err);
     res.status(500).send("Database connection error");
+  } finally {
+    await client.close();
   }
 });
 
 //POST new user
 app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect(MONGO_URL);
-    console.log('Connected successfully to server');
+  const userObj = req.body;
+  console.log(req.body);
 
+  try {
+    await client.connect();
     const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
+    const data = await db.collection("users").insertOne(userObj);
     console.log(data);
     console.log("data inserted in DB");
-    client.close();
+    res.status(201).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Unable to add user");
+  } finally {
+    await client.close();
+  }
 });
 
 
