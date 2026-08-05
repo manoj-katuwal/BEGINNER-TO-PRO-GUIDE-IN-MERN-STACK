24c658851e87c937aa2a862c95790aea236423fc
Manoj Katuwal <mkc684867@gmail.com>
2026-08-05 17:31:24 +0545

    i am tired

--- 6. Express js/Logging/logger-lab/utils/Logger.js
+++ 6. Express js/Logging/logger-lab/utils/Logger.js
@@ -1,21 +1,34 @@
 import winston from "winston";
 
- export  const logger = winston.createLogger({
+export const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-        winston.format.colorize({ all: true }), 
-
-    winston.format.timestamp(),
-    winston.format.printf(({timestamp, level, message})=> {
-        return `${timestamp} | ${level.toUpperCase()} | ${message}`;
-    })
-  ),
-
   transports: [
-    new winston.transports.Console({ forceConsoleColor: true }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `${timestamp} | ${level.toUpperCase()} | ${message}`;
+        })
+      ),
+      forceConsoleColor: true,
+    }),
 
-    new winston.transports.File({ filename: "logs/error.log", level: "error" , json: true}),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
 
-    new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.File({
+      filename: "logs/combined.log",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
   ],
 });
