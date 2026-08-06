19657a6e7b813702b3ec9e6edc0473aa0569d969
Manoj Katuwal <mkc684867@gmail.com>
2026-08-06 12:10:41 +0545

    feat: testing purpose new error setup

--- 6. Express js/devtrack-ai-backend/src/health/health.controller.js
+++ 6. Express js/devtrack-ai-backend/src/health/health.controller.js
@@ -1,3 +1,12 @@
+//  import AppError from "../utils/AppError.js";
+
+
+// FOR TESTING PURPOSES ONLY AFTER MAKING ERROR HANDLER MIDDLEWARE, YOU CAN USE THIS FUNCTION TO TEST THE ERROR HANDLER MIDDLEWARE
+// export const getHealth = (req, res, next) => {
+//   return next(new AppError("Health service is temporarily unavailable", 503));
+// };
+ 
+ 
  export const healthCheck = (req, res) => {
    res.status(200).json({
         success : true,
