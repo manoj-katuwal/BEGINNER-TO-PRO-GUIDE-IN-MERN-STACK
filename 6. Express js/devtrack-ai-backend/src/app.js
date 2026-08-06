ecbf3b51e7e57b5d122bf4435468ab3c006cbe61
Manoj Katuwal <mkc684867@gmail.com>
2026-08-06 11:50:02 +0545

    feat: add 404 handler and global error middleware

--- 6. Express js/devtrack-ai-backend/src/app.js
+++ 6. Express js/devtrack-ai-backend/src/app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import healthRoutes from "./health/health.routes.js";
-
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
+//404 handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    statusCode: 404,
+    message: `Cannot ${req.method} ${req.originalUrl} on this server`,
+  });
+});
+
+//globar error handler
+app.use(errorHandler);
+
+//routes
 app.use("/api/v1", healthRoutes);
-
-
 
 export default app;
---
+++ 6. Express js/devtrack-ai-backend/src/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+let statusCode = err.statusCode || 500;
+let status = err.status || "error";
+
+res.status(statusCode).json({
+    status: status,
+    statusCode: statusCode,
+    message: err.message
+});
+}
+
+export default errorHandler;
