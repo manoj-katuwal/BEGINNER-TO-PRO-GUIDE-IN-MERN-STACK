b592453b37c4a85e4e556b635e88104e537b90f9
Manoj Katuwal <mkc684867@gmail.com>
2026-08-06 16:56:03 +0545

    feat: add seqelize and created a mysql database

--- 6. Express js/devtrack-ai-backend/src/server.js
+++ 6. Express js/devtrack-ai-backend/src/server.js
@@ -1,10 +1,23 @@
-import app from "./app.js";
+
 import dotenv from "dotenv";
+import app from "./app.js";
+import sequelize from "./config/database.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+startServer();
